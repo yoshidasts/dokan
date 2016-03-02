ea891b5a5c6af5168ea972834505c7e82df9d31d
yoshidasts <yoshidasts@gmail.com>
2016-03-02 22:52:37 +0900

    Implement template engine. Upload template html files to S3, Mapping Template of Integration Response will be updated automatically.

--- core/view/engine.js
+++ core/view/engine.js
@@ -1,4 +1,3 @@
-var dom = require("node-dom").dom;
 var aws = require('aws-sdk');
 var s3 = new aws.S3();
 var apigateway = new aws.APIGateway();
@@ -9,11 +8,42 @@ exports.handler = function(event, context){
         Key: event.Records[0].s3.object.key
         },
         function(err, data) {
-            var html = dom(data.Body.toString(),null,{features: {removeScript: true}});
-            console.log(html);
+            var html = data.Body.toString();
+            var meta = html.split('\n')[0];
+            html = html.replace(meta, '');
+            meta = JSON.parse(meta);
             apigateway.getRestApis(function(err, data) {
-                console.log(data);
-                context.done(err, data);
+                var apiId;
+                for(var i =0; i < data.items.length; i++)
+                {
+                    if(data.items[i].name == meta.api) apiId = data.items[i].id;
+                }
+                if(!apiId) {
+                    context.done("Rest API is not found", meta.api)
+                }else{
+                    apigateway.getResources({restApiId: apiId}, function(err, data) {
+                        var resourceId;
+                        for(var i =0; i < data.items.length; i++)
+                        {
+                            if(data.items[i].path == meta.resource) resourceId = data.items[i].id;
+                        }
+                        if(!resourceId) {
+                            context.done("Resource is not found", meta.resource)
+                        }else{
+                            apigateway.putIntegrationResponse({
+                                restApiId: apiId,
+                                resourceId: resourceId,
+                                httpMethod: meta.httpMethod,
+                                statusCode: meta.statusCode,
+                                responseTemplates: { 'text/html': html}
+                                },
+                                function(err, data) {
+                                    console.log(data);
+                                    context.done(err, data);
+                            });
+                        }
+                    });
+                }
             });
         }
     );
