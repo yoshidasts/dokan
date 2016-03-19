6da479c862214ea7ad61c1d36d9a20a5d5d14652
yoshidasts <yoshidasts@gmail.com>
2016-03-19 20:22:07 +0900

    Add Credit card authentication logic

--- data/public/js/dokan_cart.js
+++ data/public/js/dokan_cart.js
@@ -6,4 +6,34 @@ $(function(){
             window.location.href = '/test/cart';
         });
     }
+    
+    // Add Credit payment to cart page
+    if($('body#cart')){
+        $(function(){
+            $('#payment-visa').on('click', function(obj){
+                var postBody  = {amount: "1", currency: "USD", payment: { cardExpirationMonth: $('#payment-visa-exmonth').attr('value'), cardExpirationYear: $('#payment-visa-exyear').attr('value'), cardNumber: $('#payment-visa-number').attr('value')}};
+                $.ajax({
+                    type : 'post',
+                    url : '/test/checkout/visa/credit',
+                    headers: {
+                        'content-type' : 'application/json'
+                    },
+                    data : JSON.stringify(postBody),
+                    success : function(data) {
+                        console.log(data);
+                        $('#payment ul').remove();
+                        $('#payment h2').after('<p>Credit Payment Succeeded.<br>Transaction id: ' + data.id + '<br>status:' + data.status + '</p>');
+                    },
+                    error : function(data) {
+                        console.log(data);
+                    }
+                });
+            });
+            
+            $("#payment li h3").on("click", function() {
+                $("#payment li dl:visible").slideToggle();
+                $(this).next().slideToggle();
+            });
+        });
+    }
 });
---
+++ payment/visa/credit_authentication.js
@@ -0,0 +1,43 @@
+var crypto = require('crypto');
+var https = require('https');
+
+var baseUri = '/cybersource/';
+var resourcePath = 'payments/v1/authorizations';
+
+exports.handler = function(event, context){
+    var postData = JSON.stringify(event.order);
+    
+    var queryParams = 'apikey=' + event.provider.apiKey;
+    var timestamp = Math.floor(Date.now() / 1000);
+    var preHashString = timestamp + resourcePath + queryParams + postData;
+    var hashString = crypto.createHmac('SHA256', event.provider.sharedSecret).update(preHashString).digest('hex');
+    var xPayToken = 'xv2:' + timestamp + ':' + hashString;
+    var data = '';
+    console.log(xPayToken);
+    var req = https.request({
+        hostname: event.provider.hostname,
+        path: baseUri + resourcePath + '?' + queryParams,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': postData.length,
+            'x-pay-token' : xPayToken
+        }
+        },function(res){
+            res.on('data', function(chunk){
+                data = data + chunk;
+            });
+            res.on('end', function() {
+                console.log(data);
+                context.done(null, JSON.parse(data));
+            });
+    });
+
+    req.on('error', function(e){
+        console.log(e);
+        context.done(e, event.code);
+    });
+    
+    req.write(postData);
+    req.end();
+}
